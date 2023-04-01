144823cc2bb829cdf9f4f292b330b89a1f2e5c61
Racconeee <raccon6614@gmail.com>
2023-04-01 23:55:50 +0900

    연결리스 link여러개로 표현한 code

---
+++ 자료구조/linkedList_link_link.java
@@ -0,0 +1,41 @@
+
+class Node {
+    String data;
+    Node link;
+
+    public Node(String name) {
+        data = name;
+        link = null;
+    }
+}
+
+public class linkedList_link_link {
+    public static void main(String[] args) {
+        System.out.println("gg");
+
+        // 첫번째 원소를 생성
+
+        Node list1;
+
+        list1 = new Node("Choi");
+        list1.link = new Node("Kim");
+        list1.link.link = new Node("Park");
+        list1.link.link.link = new Node("Yoo");
+
+        System.out.println(" " + list1.data + " " + list1.link);
+        System.out.println(" " + list1.link.data + " " + list1.link.link);
+        System.out.println(" " + list1.link.link.data + " " + list1.link.link.link);
+        System.out.println(" " + list1.link.link.link.data + " " + list1.link.link.link.link);
+
+        Node p;
+
+        p = list1;
+
+        while (p != null) {
+            System.out.print(" " + p.data);
+            p = p.link;
+        }
+        System.out.println("");
+
+    }
+}
